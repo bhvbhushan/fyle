4edbdbcf8297dd3f24e3cc3dadf1d435203f624a
bhvbhushan <bhavya.bhushan@elucidata.io>
2019-09-30 23:23:34 +0530

    [WIP] Add curl scrript to run the APIs

---
+++ get.sh
@@ -0,0 +1,3 @@
+curl --request GET https://indianbankssearch.herokuapp.com/api/bank/ABHY0065001/
+
+curl --request GET https://indianbankssearch.herokuapp.com/api/city_bank/?bank_name=ALLAHABAD+BANK&city=KOLKATA&limit=10&offset=100

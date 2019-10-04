5c516f15aeec63ce04df3ef8a3f7f11210711329
bhvbhushan <bhavya.bhushan@elucidata.io>
2019-10-05 04:25:01 +0530

    [WIP] change url for ifsc

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from .views import ListBankView, IFSCBankSearch, CityOrNameBankSearch
 
 urlpatterns = [
     path('bank/', ListBankView.as_view(), name='banks-all'),
-    path('bank/', IFSCBankSearch.as_view(), name='ifsc-detail'),
+    path('bank_ifsc/', IFSCBankSearch.as_view(), name='ifsc-detail'),
     path(r'city_bank/', CityOrNameBankSearch.as_view(), name='city-or-name-detail')
 ]
